6876ab5f201723a3b907de39ed88d96479182dee
Ranjan <sahaadi99@gmail.com>
2022-09-01 13:53:41 +0530

    First commit

---
+++ src/test/java/com/test/electronics/MsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.electronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
